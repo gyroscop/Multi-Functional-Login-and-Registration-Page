be623e0132e4529953c2d25ec5b7e8f473dc8c04
Dibya Jyoti Sarmah <dibyasarmah.work@gmail.com>
2024-01-05 08:37:59 +0530

    registration page done

--- signup/src/main/java/com/uniquedeveloper/registration/RegistrationServlet.java
+++ signup/src/main/java/com/uniquedeveloper/registration/RegistrationServlet.java
@@ -1,8 +1,12 @@
 package com.uniquedeveloper.registration;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,9 +23,56 @@ public class RegistrationServlet extends HttpServlet {
        
    
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		out.print("Working");
-		System.out.print("Working fine");
-	}
+		
+		// catch input details
+		String uname =  request.getParameter("name");
+		String upwd =  request.getParameter("pass");
+		String uemail =  request.getParameter("email");
+		String umobile =  request.getParameter("contact");
+		Connection con =  null ;
+		PreparedStatement pst = null ;
+		
+		RequestDispatcher dispatcher = null;		
+		// database connection
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/signin?useSSL=false","root","3183");
+			pst = con.prepareStatement("insert into users(uname,upwd,uemail,umobile) values(?,?,?,?)");
+			pst.setString(1, uname);
+			pst.setString(2, upwd);
+			pst.setString(3, uemail);
+			pst.setString(4, umobile);
+			
+			int rowCount = pst.executeUpdate();
+			
+			
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			
+			if (rowCount  > 0) {
+				request.setAttribute("status", "success");
+				
+			}else {
+				request.setAttribute("status", "failed");
+			}
+			
+			dispatcher.forward(request, response);
+		}catch(Exception e){
+			e.printStackTrace();		
+		}
+		finally {
+			
+			try {
+				con.close();
+				pst.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
+}
 
 }
